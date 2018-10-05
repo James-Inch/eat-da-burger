c302bf0ebe778f3f20dba43d678b877ec3de1c4d
james inch <james.inch0103@gmail.com>
2018-10-05 18:28:23 -0400

    create burger_db

---
+++ db/schema.sql
@@ -0,0 +1,12 @@
+DROP DATABASE IF EXISTS burger_db;
+CREATE DATABASE burger_db;
+USE burger_db;
+
+-- Create the table task. 
+CREATE TABLE burgers
+(
+id INt(10) NOT NULL AUTO_INCREMENT,
+burger_name VARCHAR(45) NOT NULL, 
+devoured BOOLEAN NOT NULL DEFAULT 0,
+PRIMARY KEY (id)
+);
